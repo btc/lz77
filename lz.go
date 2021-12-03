1f8fcd5d28c50bb3cf49dd3227afb84aa40f4916
btc <734339+btc@users.noreply.github.com>
2021-12-03 16:31:49 -0600

    stuff

--- lz.go
+++ lz.go
@@ -7,7 +7,47 @@ type Run struct {
 }
 
 func Encode(buf []byte) []Run {
-	return nil
+	var out []Run
+
+	for i := 0; i < len(buf); {
+
+		// a special case to simplify the code in the general case
+		// TODO re-evaluate and maybe unify
+		is_last_char := i == len(buf)-1
+		if is_last_char {
+			out = append(out, Run{0, 0, buf[i]})
+			break
+		}
+
+		found, length := 0, 0
+
+		for offset := 0; offset < i; offset++ {
+			if buf[offset] != buf[i] { // no match
+				continue
+			}
+
+			// found a match!
+
+			found, length = offset, 1
+
+			// extend the length
+			for i+length < len(buf) && found+length < i && buf[found+length] == buf[i+length] {
+				// TODO make more readable by naming cases
+				length++
+			}
+
+			break
+		}
+
+		if length == 0 {
+			out = append(out, Run{0, 0, buf[i]}) // TODO unify the append
+			i++
+		} else {
+			out = append(out, Run{found, length, buf[i+length]})
+			i += length + 1
+		}
+	}
+	return out
 }
 
 func Decode(buf []Run) []byte {
--- lz_test.go
+++ lz_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSimpleCases(t *testing.T) {
+func TestLZ77(t *testing.T) {
 
 	testCases := []struct {
 		String      string
@@ -17,12 +17,7 @@ func TestSimpleCases(t *testing.T) {
 		{
 			String:      "",
 			Runs:        nil,
-			Description: "empty string should return empty run?", // TODO confirm
-		},
-		{
-			String:      "",
-			Runs:        []Run{},
-			Description: "nil Runs should be treated the same as empty []Run value",
+			Description: "empty string should return empty run",
 		},
 		{
 			String: "a",
@@ -32,39 +27,69 @@ func TestSimpleCases(t *testing.T) {
 			Description: "single character results in single run",
 		},
 		{
+			String: "aa",
+			Runs: []Run{
+				{0, 0, 'a'}, {0, 0, 'a'},
+			},
+			Description: "repeat 2 chars",
+		},
+		{
+			String: "aaa",
+			Runs: []Run{
+				{0, 0, 'a'}, {0, 1, 'a'},
+			},
+			Description: "repeat 3 chars",
+		},
+		{
 			String: "aacaacabcaba",
 			Runs: []Run{
 				{0, 0, 'a'}, {0, 1, 'c'}, {0, 3, 'a'}, {0, 0, 'b'}, {2, 2, 'b'}, {0, 0, 'a'},
 			},
 			Description: "the test case Sean & I ran through together",
 		},
+		{
+			String:      "aXaaXaaaXY",
+			Runs:        []Run{},
+			Description: "TODO: fill in the runs for this greedy case",
+			Skip:        true,
+		},
 	}
 	for _, testCase := range testCases {
-		t.Run(testCase.Description, func(t *testing.T) {
+		t.Run(testCase.Description+":"+testCase.String, func(t *testing.T) {
+			if testCase.Skip {
+				t.SkipNow()
+			}
 
 			t.Logf(`String: "%s"`, testCase.String)
 
-			decoded := Decode(testCase.Runs)
-			require.Equal(t,
-				testCase.String, string(decoded),
-				"Decode: the decoded runs should match the input string")
+			t.Run("Decode", func(t *testing.T) {
+				decoded := Decode(testCase.Runs)
+				require.Equal(t,
+					testCase.String, string(decoded),
+					"Decode: the decoded value should match the input string")
+			})
 
-			return
+			t.Run("Encode", func(t *testing.T) {
+				encoded := Encode([]byte(testCase.String))
+				require.Len(t,
+					encoded, len(testCase.Runs),
+					"Encode: should result in Runs of length equal to the testCase.Runs")
+				require.Equal(t,
+					testCase.Runs, encoded,
+					"Encode: the encoded value should match the testCase Runs")
+			})
 
-			encoded := Encode([]byte(testCase.String))
-			require.Equal(t,
-				testCase.Runs, encoded,
-				"Encode: the encoded String should match the testCase Runs")
+			t.Run("Roundtrip", func(t *testing.T) {
+				str := Decode(Encode([]byte(testCase.String)))
+				require.Equal(t,
+					testCase.String, string(str),
+					"Roundtrip: Strings should come back out the same after (Encode -> Decode) operation")
 
-			str := Decode(Encode([]byte(testCase.String)))
-			require.Equal(t,
-				testCase.String, string(str),
-				"Roundtrip: Strings should come back out the same after (Encode -> Decode) operation")
-
-			runs := Encode(Decode(testCase.Runs))
-			require.Equal(t,
-				testCase.Runs, runs,
-				"Roundtrip: Runs should come back out the same after (Decode -> Encode) operation")
+				runs := Encode(Decode(testCase.Runs))
+				require.Equal(t,
+					testCase.Runs, runs,
+					"Roundtrip: Runs should come back out the same after (Decode -> Encode) operation")
+			})
 		})
 	}
 }
